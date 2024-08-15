6f2da434aecd03034aeed7ed0666645b4fd35bff
Sree Deekshitha Yerra <126445402+SreeEswaran@users.noreply.github.com>
2024-08-15 23:02:41 +0530

    Update image_model.py

--- scripts/models/image_model.py
+++ scripts/models/image_model.py
@@ -6,7 +6,6 @@ class ImageModel:
     def __init__(self):
         self.feature_extractor = ViTFeatureExtractor.from_pretrained('google/vit-base-patch16-224')
         self.model = ViTForImageClassification.from_pretrained('google/vit-base-patch16-224')
-
     def predict(self, image):
         inputs = self.feature_extractor(images=image, return_tensors="pt")
         with torch.no_grad():
